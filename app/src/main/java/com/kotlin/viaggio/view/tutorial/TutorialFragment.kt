b81877b46255c51137802ccf07c03969e31b895c
minkyu <minkyu9403@gmail.com>
2019-02-12 12:04:42 +0900

    minkyu - tutorial 무한 루프 viewpager 오른쪽 구현

--- app/src/main/java/com/kotlin/viaggio/view/tutorial/TutorialFragment.kt
+++ app/src/main/java/com/kotlin/viaggio/view/tutorial/TutorialFragment.kt
@@ -29,33 +29,39 @@ class TutorialFragment:BaseFragment<TutorialFragmentViewModel>() {
             tutorialPager.adapter = object: RecyclerView.Adapter<RecyclerView.ViewHolder>(){
                 override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
                     TutorialViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_tutorial, parent, false))
-                override fun getItemCount() = it.size
+                override fun getItemCount() = it.size + 1
                 override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
                     holder as TutorialViewHolder
-                    holder.binding?.data = it[position]
+                    if(position < it.size){
+                        holder.binding?.data = it[position]
 
-                    holder.itemView.tutorialAnim.apply {
-                        setAnimation(it[position].animRes)
-                        addAnimatorListener(object : Animator.AnimatorListener{
-                            override fun onAnimationRepeat(animation: Animator?) {}
-                            override fun onAnimationCancel(animation: Animator?) {}
-                            override fun onAnimationStart(animation: Animator?) {}
-                            override fun onAnimationEnd(animation: Animator?) {
-                                tutorialPager.currentItem = if(position < it.size-1) position + 1 else 0
-                            }
-                        })
+                        holder.itemView.tutorialAnim.apply {
+                            setAnimation(it[position].animRes)
+                            addAnimatorListener(object : Animator.AnimatorListener{
+                                override fun onAnimationRepeat(animation: Animator?) {}
+                                override fun onAnimationCancel(animation: Animator?) {}
+                                override fun onAnimationStart(animation: Animator?) {}
+                                override fun onAnimationEnd(animation: Animator?) {
+                                    tutorialPager.currentItem = if(position < it.size-1) position + 1 else 0
+                                }
+                            })
+                        }
                     }
                 }
             }
             tutorialPagerIndicator.setCurrPageNumber(0)
             tutorialPagerIndicator.setTotalPageNumber(it.size)
-        })
 
-        tutorialPager.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                tutorialPagerIndicator.setCurrPageNumber(position)
-            }
+            tutorialPager.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    if(position < it.size) {
+                        super.onPageSelected(position)
+                        tutorialPagerIndicator.setCurrPageNumber(position)
+                    }else{
+                        tutorialPager.currentItem = 0
+                    }
+                }
+            })
         })
     }
 
