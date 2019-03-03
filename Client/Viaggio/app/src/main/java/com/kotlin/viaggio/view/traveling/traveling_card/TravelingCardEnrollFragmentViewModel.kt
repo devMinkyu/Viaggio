dce701f97a3ff52564dca242ea74f7849354ac30
minkyu <minkyu9403@gmail.com>
2019-03-03 17:11:54 +0900

    minkyu - travelCard enroll day auto setting error fix

--- Client/Viaggio/app/src/main/java/com/kotlin/viaggio/view/traveling/traveling_card/TravelingCardEnrollFragmentViewModel.kt
+++ Client/Viaggio/app/src/main/java/com/kotlin/viaggio/view/traveling/traveling_card/TravelingCardEnrollFragmentViewModel.kt
@@ -8,12 +8,14 @@ import androidx.databinding.ObservableInt
 import androidx.lifecycle.MutableLiveData
 import com.kotlin.viaggio.R
 import com.kotlin.viaggio.data.`object`.TravelCard
+import com.kotlin.viaggio.data.`object`.TravelOfDay
 import com.kotlin.viaggio.data.source.AndroidPrefUtilService
 import com.kotlin.viaggio.event.Event
 import com.kotlin.viaggio.model.TravelModel
 import com.kotlin.viaggio.view.common.BaseViewModel
 import io.reactivex.Single
 import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Function3
 import io.reactivex.schedulers.Schedulers
 import java.text.SimpleDateFormat
 import java.util.*
@@ -62,22 +64,26 @@ class TravelingCardEnrollFragmentViewModel @Inject constructor() : BaseViewModel
     }
 
     fun saveTravelCard() {
-        val cal = Calendar.getInstance()
-        cal.set(Calendar.HOUR_OF_DAY, time.get()!!.split(":")[0].toInt())
-        cal.set(Calendar.MINUTE, time.get()!!.split(":")[1].toInt())
 
         val imagePathSingle = travelModel.imagePathList(imageChooseList)
-        val orderTravelCard = travelModel.getTravelCards()
+        val orderTravelCardSingle = travelModel.getTravelCards()
+        val travelOfDaySingle = travelModel.getTravelOfDay()
 
         val disposable = Single.zip(
             imagePathSingle,
-            orderTravelCard,
-            BiFunction<List<String>, List<TravelCard>, TravelCard> { t1, t2 ->
+            orderTravelCardSingle,
+            travelOfDaySingle,
+            Function3<List<String>, List<TravelCard>,TravelOfDay, TravelCard>  { t1, t2, t3 ->
                 val imageNames: MutableList<String> = mutableListOf()
                 for (s in t1) {
                     imageNames.add(Uri.parse(s).lastPathSegment!!)
                 }
                 val order = if (t2.isNullOrEmpty()) 1 else t2.size + 1
+
+                val cal = Calendar.getInstance()
+                cal.time = t3.date
+                cal.set(Calendar.HOUR_OF_DAY, time.get()!!.split(":")[0].toInt())
+                cal.set(Calendar.MINUTE, time.get()!!.split(":")[1].toInt())
                 val travelCard = TravelCard(
                     country = prefUtilService.getString(AndroidPrefUtilService.Key.TRAVELING_LAST_COUNTRIES).blockingGet(),
                     contents = contents.get()!!,
@@ -89,14 +95,9 @@ class TravelingCardEnrollFragmentViewModel @Inject constructor() : BaseViewModel
                     imageNames = imageNames as ArrayList<String>
                 )
                 if (order == 1) {
-                    val disposable = travelModel.getTravelOfDay().subscribe({ travelOfCard ->
-                        travelOfCard.themeImageName = imageNames[0]
-                        travelModel.updateTravelOfDay(travelOfCard)
-                        rxEventBus.travelOfDayImage.onNext(travelOfCard.themeImageName)
-                    }) {
-
-                    }
-                    addDisposable(disposable)
+                    t3.themeImageName = imageNames[0]
+                    travelModel.updateTravelOfDay(t3)
+                    rxEventBus.travelOfDayImage.onNext(t3.themeImageName)
                 }
                 travelCard
             }).flatMap { travelCard ->
