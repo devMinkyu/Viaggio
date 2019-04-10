e0ae3e6a7ea4a507f81b4ac5284e3b248e6bf6d2
Yongsung Heo <ysheo94@tag-hive.com>
2019-04-10 16:31:56 +0900

    401, 403 common error logics are added. Auth before_request logic is added.

--- viaggio-server/app/api/users.py
+++ viaggio-server/app/api/users.py
@@ -3,21 +3,27 @@ from . import api
 from .. import db
 from ..models import User
 from ..forms.user import ChangePasswordForm, ChangeUserInfoForm
-from ..errors import bad_request
-import uuid
+from ..errors import bad_request, unauthorized
+
+
+@api.before_request
+def before_request():
+    if request.headers.get('authorization') is None:
+        return unauthorized('Token is not exist.')
+    if User.query.filter_by(token=request.headers['authorization']).first() is None:
+        return unauthorized('There is no user matched with token.')
+    else:
+        request.user = User.query.filter_by(token=request.headers['authorization']).first()
 
 
 @api.route('/users/changepwd', methods=['POST'])
 def change_password():
     form = ChangePasswordForm(request.form)
     if form.validate():
-        # Todo: Exception When token is not exist.
-        user = User.query.filter_by(token=request.headers['authorization'],
-                                    passwordHash=request.form['oldPasswordHash']).first()
-        if user is None:
+        if request.user is None:
             return bad_request(401, 'There is no user matched with token and pwd when change user pwd.')
-        user.passwordHash = request.form['passwordHash']
-        db.session.add(user)
+        request.user.passwordHash = request.form['passwordHash']
+        db.session.add(request.user)
         db.session.commit()
         return jsonify({ 'result': 'User password is changed.' }), 200
     
@@ -31,16 +37,14 @@ def change_password():
 def change_name():
     form = ChangeUserInfoForm(request.form)
     if form.validate():
-        # Todo: Exception When token is not exist.
-        user = User.query.filter_by(token=request.headers['authorization']).first()
-        if user is None:
+        if request.user is None:
             return bad_request(401, 'There is no user matched with token when change user name.')
-        user.name = request.form['name']
+        request.user.name = request.form['name']
         if request.form.get('profileImageName') is not None \
                 and request.form.get('profileImageUrl') is not None:
-            user.profileImageName = request.form['profileImageName']
-            user.profileImageUrl = request.form['profileImageUrl']
-        db.session.add(user)
+            request.user.profileImageName = request.form['profileImageName']
+            request.user.profileImageUrl = request.form['profileImageUrl']
+        db.session.add(request.user)
         db.session.commit()
         return jsonify({ 'result': 'User info is changed.' }), 200
     
@@ -49,11 +53,10 @@ def change_name():
 
 @api.route('/users/logout')
 def logout():
-    user = User.query.filter_by(token=request.headers['authorization']).first()
-    if user is None:
+    if request.user is None:
         return jsonify({ 'result': 'Token is already null.' }), 200
 
-    user.token = None
-    db.session.add(user)
+    request.user.token = None
+    db.session.add(request.user)
     db.session.commit()
     return jsonify({ 'result': 'User logout is success.' }), 200
--- viaggio-server/app/errors.py
+++ viaggio-server/app/errors.py
@@ -5,13 +5,9 @@ def bad_request(message, detail):
     return jsonify({ 'message': message, 'detail': detail }), 400
 
 
-def unauthorized(message, detail):
-    response = jsonify({ 'error': 'unauthorized', 'detail': detail })
-    response.status_code = 401
-    return response
+def unauthorized(detail):
+    return jsonify({ 'message': 'unauthorized', 'detail': detail }), 401
 
 
-def forbidden(message, detail):
-    response = jsonify({ 'error': 'forbidden', 'detail': detail })
-    response.status_code = 403
-    return response
+def forbidden(detail):
+    return jsonify({ 'message': 'forbidden', 'detail': detail }), 403
