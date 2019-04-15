fe6573d43c87a9efdad803e60873c34089446715
Yongsung Heo <ysheo94@tag-hive.com>
2019-04-15 22:36:39 +0900

    Travel validation form and APIs are added.

---
+++ viaggio-server/app/forms/travel.py
@@ -0,0 +1,21 @@
+from wtforms import Form, StringField, DateTimeField, SelectField
+from wtforms import ValidationError
+from wtforms.validators import DataRequired, length
+from ..models import User
+
+
+class CreateTravelForm(Form):
+    startDate = DateTimeField('StartDate', validators=[DataRequired()])
+    travelType = SelectField('TravelType', choices=[('preDomestic', 'preDomestic'),
+                                                    ('preForeign', 'preForeign'),
+                                                    ('domestic', 'domestic'),
+                                                    ('foreign', 'foreign')])
+    entireCountry = StringField('EntireCountry', validators=[DataRequired()])
+
+
+class UpdateTravelForm(Form):
+    travelType = SelectField('TravelType', choices=[('preDomestic', 'preDomestic'),
+                                                    ('preForeign', 'preForeign'),
+                                                    ('domestic', 'domestic'),
+                                                    ('foreign', 'foreign')])
+    endDate = DateTimeField('EndDate', validators=[])
