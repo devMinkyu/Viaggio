bd95d5e563c6ce59ec6a050de659e4990da7a69b
Yongsung Heo <ysheo94@tag-hive.com>
2019-05-08 23:30:03 +0900

    Create TravelCard form validation is added.

---
+++ viaggio-server/app/forms/travelcard.py
@@ -0,0 +1,14 @@
+from wtforms import Form, StringField, DateTimeField, SelectField
+from wtforms import ValidationError
+from wtforms.validators import DataRequired, length
+from ..models import TravelCard
+
+
+class CreateTravelCardForm(Form):
+    localId = IntegerField('LocalId', validators=[DataRequired()])
+    travelLocalId = IntegerField('TravelLocalId', validators=[DataRequired()])
+    travelOfDay = IntegerField('TravelOfDay', validators=[DataRequired()])
+
+    def validate_localId(self, field):
+        if TravelCard.query.filter_by(localId=field.data).first():
+            raise ValidationError('LocalId already exist.')
