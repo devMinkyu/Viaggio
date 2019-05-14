be4539981d97a690c15e0eb9cce6743507ec1ffa
Yongsung Heo <ysheo94@tag-hive.com>
2019-05-14 15:09:58 +0900

    Login, signup and get aws token logic is separated.

---
+++ viaggio-server/app/api/aws.py
@@ -0,0 +1,18 @@
+from flask import jsonify, request
+from . import api
+from .. import db
+from ..models import User
+from ..errors import bad_request
+
+
+@api.route('/my/aws')
+def aws():
+    user = User.query.filter_by(token=request.headers.get('authorization')).first()
+    if user is None:
+        return bad_request(401, 'There is no user matched with token.')
+
+    try:
+        response = user.get_aws_token()
+        return jsonify({ 'AWS_IdentityId': response['IdentityId'], 'AWS_Token': response['Token'] }), 200
+    except:
+        return bad_request(402, 'Get AWS validation is failed.')
