0f6d44b405097b19c53b635dcd41e99884d1dd71
Yongsung Heo <ysheo94@tag-hive.com>
2019-05-21 16:29:33 +0900

    Update Travel and TravelCard logic and validation is modified.

--- viaggio-server/app/forms/travel.py
+++ viaggio-server/app/forms/travel.py
@@ -13,8 +13,3 @@ class CreateTravelForm(Form):
     def validate_localId(self, field):
         if Travel.query.filter_by(localId=field.data).first():
             raise ValidationError('LocalId already exist.')
-
-
-class UpdateTravelForm(Form):
-    travelKind = SelectField('TravelKind', coerce=int, choices=[(0, 'foreign'),
-                                                                (1, 'domestic')])
