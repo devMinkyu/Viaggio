9a77dbfcbbb8b799b11ed7316c5b9256278fb3e8
Yongsung Heo <ysheo94@gmail.com>
2019-05-30 11:35:32 +0900

    Common file is separated.

--- viaggio-server/app/commons/common.py
+++ viaggio-server/app/commons/common.py
@@ -1,26 +1,25 @@
 from flask import jsonify, request, json
-from . import api
+from . import commons
 from .. import db
-from ..models import Travel, TravelCard
 from ..errors import bad_request
 import os
 
 
-@api.route('/common/countries')
+@commons.route('/countries')
 def get_contries():
     data = json.load(open('app/static/country.json'))
 
     return jsonify(data), 200
 
 
-@api.route('/common/domestics')
+@commons.route('/domestics')
 def get_domestics():
     data = json.load(open('app/static/domestic.json'))
 
     return jsonify({ 'domestics': data }), 200
 
 
-@api.route('/common/themes')
+@commons.route('/themes')
 def get_themes():
     data = json.load(open('app/static/theme.json'))
 
