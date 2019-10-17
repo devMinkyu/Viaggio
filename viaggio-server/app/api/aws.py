33c9c4d90caa2e4e85c8d194e58250490a776820
Yongsung Heo <ysheo94@gmail.com>
2019-10-17 22:14:54 +0900

    Get aws API error is fixed.

--- viaggio-server/app/api/aws.py
+++ viaggio-server/app/api/aws.py
@@ -7,12 +7,15 @@ from ..errors import bad_request
 
 @api.route('/my/aws')
 def aws():
-    user = User.query.filter_by(token=request.headers.get('authorization')).first()
+    user = User.query.filter_by(token=request.headers.get('auth')).first()
     if user is None:
         return bad_request(401, 'There is no user matched with token.')
 
     try:
         response = user.get_aws_token()
-        return jsonify({ 'AWS_IdentityId': response['IdentityId'], 'AWS_Token': response['Token'] }), 200
     except:
         return bad_request(402, 'Get AWS validation is failed.')
+    else:
+        return jsonify({
+            'AWS_IdentityId': response['IdentityId'],
+            'AWS_Token': response['Token'] }), 200
